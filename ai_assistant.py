0823514c832e8904f0fd28999579c4ceb824262b
GuguAILab <amarendra.swain0404@gmail.com>
2026-06-28 17:59:41 +0530

    Update ai_assistant.py
    test

--- ai_assistant.py
+++ ai_assistant.py
@@ -766,7 +766,7 @@ if not st.session_state["logged_in"]:
     import base64
 
     _nit_img_tag = ""
-    _nit_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Nit.png")
+    _nit_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Robot.png")
     if os.path.exists(_nit_path):
         with open(_nit_path, "rb") as _f:
             _nit_b64 = base64.b64encode(_f.read()).decode("utf-8")
